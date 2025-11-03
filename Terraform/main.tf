e5da937319a85c4f475e0f43f89653e7705b24ed
DevMahbubHussain <devmahbubmamun@gmail.com>
2025-11-03 11:20:53 +0600

    updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,12 +18,26 @@ locals {
 #   }
 # }
 
-resource "aws_key_pair" "web_key" {
-  key_name   = "web_key"
-  public_key = file("~/.ssh/web_key.pub")
+resource "tls_private_key" "web_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+resource "aws_key_pair" "web_key" {
+  key_name   = "web_key"
+  public_key = tls_private_key.web_key.public_key_openssh
+}
 
+resource "local_file" "private_key" {
+  content         = tls_private_key.web_key.private_key_pem
+  filename        = "${path.cwd}/web_key.pem"
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.web_key.public_key_openssh
+  filename = "${path.cwd}/web_key.pub"
+}
 
 
 data "aws_ami" "ubuntu" {
